3df6b01e770e9359004bdf0a6e579dfab099c206
Zheekka <165438238+Zheekka@users.noreply.github.com>
2024-04-22 00:57:27 +0700

    Update homework10.py

--- homework10.py
+++ homework10.py
@@ -4,8 +4,8 @@ def test(*params):
 test(1,"строка",True,8 )
 
 def factorial_recursive(n):
-    if n == 1:
-        return n
+    if n == 0 or n == 1:
+        return 1
     else:
         return n*factorial_recursive(n-1)
 
